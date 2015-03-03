6d07d1a6524f9354941262629af1b2764ee257ad
Peter Butterfill <pete88b@users.noreply.github.com>
2015-03-03 11:57:02 +0000

    minor change

--- sql-runner/src/main/java/com/butterfill/sqlrunner/SqlRunnerFactory.java
+++ sql-runner/src/main/java/com/butterfill/sqlrunner/SqlRunnerFactory.java
@@ -31,7 +31,7 @@ public class SqlRunnerFactory {
             new AttributeSettingResultSetNextRowCallbackHandlerImpl();
 
     /**
-     * The name of the character set of the SQL script file.
+     * Name of the character set of SQL files.
      */
     private String charsetName = "UTF-8";
 
@@ -72,93 +72,219 @@ public class SqlRunnerFactory {
      */
     private Map<String, SqlRunnerResultSetNextRowCallbackHandler> rsnrCallbackHandlerMap;
 
+    /**
+     * Returns the datasource to be used by SqlRunners created by this factory.
+     * @return
+     *   The datasource to be used by SqlRunners created by this factory.
+     */
     public DataSource getDataSource() {
         return dataSource;
     }
 
+    /**
+     * Sets the datasource to be used by SqlRunners created by this factory.
+     * @param dataSource
+     *   The datasource to be used by SqlRunners created by this factory.
+     */
     public void setDataSource(final DataSource dataSource) {
         this.dataSource = dataSource;
     }
 
+    /**
+     * Returns the default callback handler to be used by SqlRunners created by this factory.
+     * @return
+     *   The default callback handler to be used by SqlRunners created by this factory.
+     */
     public SqlRunnerCallbackHandler getDefaultCallbackHandler() {
         return defaultCallbackHandler;
     }
 
+    /**
+     * Sets the default callback handler to be used by SqlRunners created by this factory.
+     * @param defaultCallbackHandler
+     *   The default callback handler to be used by SqlRunners created by this factory.
+     */
     public void setDefaultCallbackHandler(final SqlRunnerCallbackHandler defaultCallbackHandler) {
         this.defaultCallbackHandler = defaultCallbackHandler;
     }
 
+    /**
+     * Returns the default result set handler to be used by SqlRunners created by this factory.
+     * @return
+     *   The default result set handler to be used by SqlRunners created by this factory.
+     */
     public SqlRunnerResultSetNextRowCallbackHandler getDefaultResultSetNextRowCallbackHandler() {
         return defaultResultSetNextRowCallbackHandler;
     }
 
+    /**
+     * Sets the default result set handler to be used by SqlRunners created by this factory.
+     * @param defaultResultSetNextRowCallbackHandler
+     *   The default result set handler to be used by SqlRunners created by this factory.
+     */
     public void setDefaultResultSetNextRowCallbackHandler(
             final SqlRunnerResultSetNextRowCallbackHandler defaultResultSetNextRowCallbackHandler) {
         this.defaultResultSetNextRowCallbackHandler = defaultResultSetNextRowCallbackHandler;
     }
 
+    /**
+     * Returns the name of the character set to be used by SqlRunners created by this factory.
+     * SqlRunners use the character set name when reading files.
+     * @return
+     *   The name of the character set to be used by SqlRunners created by this factory.
+     */
     public String getCharsetName() {
         return charsetName;
     }
 
+    /**
+     * Sets the name of the character set to be used by SqlRunners created by this factory.
+     * SqlRunners use the character set name when reading files.
+     * @param charsetName
+     *   The name of the character set to be used by SqlRunners created by this factory.
+     */
     public void setCharsetName(final String charsetName) {
         this.charsetName = charsetName;
     }
 
+    /**
+     * Returns the file path prefix to be used by SqlRunners created by this factory.
+     * SqlRunners use the file path prefix when reading files.
+     * @return
+     *   The file path prefix to be used by SqlRunners created by this factory.
+     */
     public String getFilePathPrefix() {
         return filePathPrefix;
     }
 
+    /**
+     * Sets the file path prefix to be used by SqlRunners created by this factory.
+     * SqlRunners use the file path prefix when reading files.
+     * @param filePathPrefix
+     *   The file path prefix to be used by SqlRunners created by this factory.
+     */
     public void setFilePathPrefix(final String filePathPrefix) {
         this.filePathPrefix = filePathPrefix;
     }
 
+    /**
+     * Returns the attribute name prefix to be used by SqlRunners created by this factory.
+     * SqlRunners use the attribute name prefix when replacing attributes files.
+     * @return
+     *   The attribute name prefix to be used by SqlRunners created by this factory.
+     */
     public String getAttributeNamePrefix() {
         return attributeNamePrefix;
     }
 
+    /**
+     * Sets the attribute name prefix to be used by SqlRunners created by this factory.
+     * SqlRunners use the attribute name prefix when replacing attributes files.
+     * @param attributeNamePrefix
+     *   The attribute name prefix to be used by SqlRunners created by this factory.
+     */
     public void setAttributeNamePrefix(final String attributeNamePrefix) {
         this.attributeNamePrefix = attributeNamePrefix;
     }
 
+    /**
+     * Returns the attribute name postfix to be used by SqlRunners created by this factory.
+     * SqlRunners use the attribute name postfix when replacing attributes files.
+     * @return
+     *   The attribute name postfix to be used by SqlRunners created by this factory.
+     */
     public String getAttributeNamePostfix() {
         return attributeNamePostfix;
     }
 
+    /**
+     * Sets the attribute name postfix to be used by SqlRunners created by this factory.
+     * SqlRunners use the attribute name postfix when replacing attributes files.
+     * @param attributeNamePostfix
+     *   The attribute name postfix to be used by SqlRunners created by this factory.
+     */
     public void setAttributeNamePostfix(final String attributeNamePostfix) {
         this.attributeNamePostfix = attributeNamePostfix;
     }
 
+    /**
+     * Returns the single line comment prefix to be used by SqlRunners created by this factory.
+     * SqlRunners use the single line comment prefix when reading files.
+     * @return
+     *   The single line comment prefix to be used by SqlRunners created by this factory.
+     */
     public String getSingleLineCommentPrefix() {
         return singleLineCommentPrefix;
     }
 
-    public void setSingleLineCommentPrefix(String singleLineCommentPrefix) {
+    /**
+     * Sets the single line comment prefix to be used by SqlRunners created by this factory.
+     * SqlRunners use the single line comment prefix when reading files.
+     * @param singleLineCommentPrefix
+     *   The single line comment prefix to be used by SqlRunners created by this factory.
+     */
+    public void setSingleLineCommentPrefix(final String singleLineCommentPrefix) {
         this.singleLineCommentPrefix = singleLineCommentPrefix;
     }
 
+    /**
+     * Returns the attribute map of this instance - all attributes in this map are set on
+     * SqlRunners by {@link #newSqlRunner() }.
+     * @return
+     *   The attribute map of this instance.
+     */
     public Map<String, String> getAttributeMap() {
         return attributeMap;
     }
 
+    /**
+     * Sets the attribute map of this instance - all attributes in this map are set on
+     * SqlRunners by {@link #newSqlRunner() }.
+     * @param attributeMap
+     *   The attribute map of this instance.
+     */
     public void setAttributeMap(final Map<String, String> attributeMap) {
         this.attributeMap = attributeMap;
     }
 
+    /**
+     * Returns the callback handler map to be used by SqlRunners created by this factory.
+     * SqlRunners use callback handlers when executing SQL statements.
+     * @return
+     *   The callback handler map to be used by SqlRunners created by this factory.
+     */
     public Map<String, SqlRunnerCallbackHandler> getCallbackHandlerMap() {
         return callbackHandlerMap;
     }
 
+    /**
+     * Sets the callback handler map to be used by SqlRunners created by this factory.
+     * SqlRunners use callback handlers when executing SQL statements.
+     * @param callbackHandlerMap
+     *   The callback handler map to be used by SqlRunners created by this factory.
+     */
     public void setCallbackHandlerMap(
             final Map<String, SqlRunnerCallbackHandler> callbackHandlerMap) {
         this.callbackHandlerMap = callbackHandlerMap;
     }
 
+    /**
+     * Returns the result set handler map to be used by SqlRunners created by this factory.
+     * SqlRunners use result set handlers when processing result sets.
+     * @return
+     *   The result set handler map to be used by SqlRunners created by this factory.
+     */
     public Map<String, SqlRunnerResultSetNextRowCallbackHandler>
         getRunnerResultSetNextRowCallbackHandlerMap() {
         return rsnrCallbackHandlerMap;
     }
 
+    /**
+     * Sets the result set handler map to be used by SqlRunners created by this factory.
+     * SqlRunners use result set handlers when processing result sets.
+     * @param rsnrCallbackHandlerMap
+     *   The result set handler map to be used by SqlRunners created by this factory.
+     */
     public void setRunnerResultSetNextRowCallbackHandlerMap(
             final Map<String, SqlRunnerResultSetNextRowCallbackHandler> rsnrCallbackHandlerMap) {
         this.rsnrCallbackHandlerMap = rsnrCallbackHandlerMap;
